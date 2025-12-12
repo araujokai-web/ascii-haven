b483686ca45e3f61831c4d3e860502cdce3daffd
araujokai-web <io.araujokai@gmail.com>
2025-12-12 08:35:27 +0000

    changes

--- Assets/Scripts/ASCIIinator.cs
+++ Assets/Scripts/ASCIIinator.cs
@@ -10,11 +10,16 @@ public class ASCIIinator : MonoBehaviour
 
 {
     public SpriteRenderer spriteRenderer; 
-        
+    
     private const string brightnessString = "Ñ@#W$9876543210?!abc;:+=-,._ ";
-
+    
     private string output;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
     public void ASCIIinate() {
         Color pixel = Color.white;
         float gray = Random.Range(0f, 1f);
@@ -22,9 +27,6 @@ public class ASCIIinator : MonoBehaviour
         int index = gray.Remap(0, 1, 0, brightnessString.Length - 1);
         char ASCIICHARCTER = brightnessString[index];
         output += $"<color=#{pixel.ConvertToHex()}>";
-        //Need to use Texture2D
     }
-
-    // Update is called once per frame
-
+    
 }
---
+++ Assets/Scripts/notes.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class notes : MonoBehaviour
+
+{
+    public string myName = " none";
+
+    void Start()
+    {
+        Debug.Log("this is not a machine " + myName);
+    }
+}
