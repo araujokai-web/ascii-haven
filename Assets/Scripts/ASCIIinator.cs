f7a6ad40effae0fde865403b7134b99840bc6ad0
araujokai-web <io.araujokai@gmail.com>
2025-12-12 16:26:43 +0000

    Ascii characters imported

--- Assets/Scripts/ASCIIinator.cs
+++ Assets/Scripts/ASCIIinator.cs
@@ -1,5 +1,6 @@
 using System;
 using grayScale;
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,18 +10,37 @@ using Random = UnityEngine.Random;
 public class ASCIIinator : MonoBehaviour
 
 {
-    public SpriteRenderer spriteRenderer; 
+    public Sprite sprite;
+    public TMP_InputField Textext;
     
     private const string brightnessString = "Ñ@#W$9876543210?!abc;:+=-,._ ";
     
     private string output;
+
     void Start()
     {
-    
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        Texture2D texture = sprite.texture;
+        output = "";
+        for (int y = texture.height-1; y > 0; y--)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                Color color = texture.GetPixel(x, y);
+                float gray = color.r * 0.299f + color.g * 0.587f + color.b * 0.114f;
+                int index = gray.Remap(0, 1, brightnessString.Length - 1, 0);
+                output += brightnessString[index];
+            }
+
+            output += "\n";
+        }
+
+        Textext.text = output;
+
+    Color [,] grayScalePixels = new Color[3, 3];
+            
         
     }
-    public void ASCIIinate() {
+    public void ASCIIinate() {  
         Color pixel = Color.white;
         float gray = Random.Range(0f, 1f);
         char N = brightnessString[1];
--- Assets/Scripts/notes.cs
+++ Assets/Scripts/notes.cs
@@ -11,4 +11,7 @@ public class notes : MonoBehaviour
     {
         Debug.Log("this is not a machine " + myName);
     }
-}
+}//maybe texture2D because he is trying to get the information out 
+//Color pixel = spriteRenderer.GetPixels(0, 0, 1, 1); //maybe texture 2D but .image does not make sense
+//float gray = pixel.r * 0.299f + pixel.g * 0.587f + pixel.b * 0.114f;        
+//Color grayScalePixel = new Color(pixel.r, pixel.g, pixel.b, gray);
